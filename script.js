e1eca1b22c803d7c1c8f0912878f2ce27600d745
py-glich <mb278427@gmail.com>
2026-05-21 14:26:26 +0500

    Add live calculations

---
+++ script.js
@@ -0,0 +1,30 @@
+const billInput = document.getElementById("bill");
+
+const peopleInput = document.getElementById("people");
+
+const customTipInput =
+  document.getElementById("custom-tip");
+
+const tipButtons =
+  document.querySelectorAll(".tip-btn");
+
+const tipAmountText =
+  document.getElementById("tip-amount");
+
+const grandTotalText =
+  document.getElementById("grand-total");
+
+const perPersonText =
+  document.getElementById("per-person");
+
+const billError =
+  document.getElementById("bill-error");
+
+const tipError =
+  document.getElementById("tip-error");
+
+const peopleError =
+  document.getElementById("people-error");
+
+const resetBtn =
+  document.getElementById("reset-btn");
